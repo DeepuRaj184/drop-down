1c67d53c5960a142f79bb13c4d7771ccf8065d03
DeepuRaj184 <141441321+DeepuRaj184@users.noreply.github.com>
2024-04-27 22:39:22 +0530

    Update Dropdisplay.jsx

--- src/Dropdisplay.jsx
+++ src/Dropdisplay.jsx
@@ -1,18 +1,18 @@
-import { Dropvalue } from "./dropcontext"
-import styles from "./css/Dropdisplay.module.css"
+import { Dropvalue } from "./dropcontext" // Importing the custom hook Dropvalue from the dropcontext file
+import styles from "./css/Dropdisplay.module.css" // Importing CSS module styles from Dropdisplay.module.css
 
-function Dropdisplay(){
-    const {drop,clearDropdown}=Dropvalue()
+function Dropdisplay(){ // Defining a functional component named Dropdisplay
+    const {drop,clearDropdown}=Dropvalue() // Destructuring values from the Dropvalue hook
+
     return(
         <>
-            <ul>
-                {drop.map((car,i)=>(
-                    <li onClick={clearDropdown}  className={styles.cars} key={i}><a>{car}</a></li>
+            <ul> {/* Rendering an unordered list */}
+                {drop.map((car,i)=>( // Mapping over the 'drop' array to render list items for each car
+                    <li onClick={clearDropdown}  className={styles.cars} key={i}><a>{car}</a></li> // List item with a click event listener to clear the dropdown and applying CSS class 'cars'
                 ))}
             </ul>
         </>
-        
     )
 }
 
-export default Dropdisplay
+export default Dropdisplay; // Exporting the Dropdisplay component as default
