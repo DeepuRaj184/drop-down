d6c49787be15652c9fff6e2a549174819b4e0db4
DeepuRaj184 <141441321+DeepuRaj184@users.noreply.github.com>
2024-04-27 22:38:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import Dropdown from "./Dropdown";
-import { DropContext } from "./dropcontext";
-import Dropdisplay from "./Dropdisplay";
+import Dropdown from "./Dropdown"; // Importing the Dropdown component from Dropdown.js
+import { DropContext } from "./dropcontext"; // Importing the DropContext component from dropcontext.js
+import Dropdisplay from "./Dropdisplay"; // Importing the Dropdisplay component from Dropdisplay.js
 
-function App() {
+function App() { // Defining a functional component named App
   return (
-    <DropContext>
-      <Dropdown />
-      <Dropdisplay />
+    <DropContext> {/* Providing the DropContext component */}
+      <Dropdown /> {/* Rendering the Dropdown component */}
+      <Dropdisplay /> {/* Rendering the Dropdisplay component */}
     </DropContext>
-    
   );
 }
 
-export default App;
+export default App; // Exporting the App component as default
