5c18c7af27ab42ba21e3f4409c49fb9022751db6
DeepuRaj184 <141441321+DeepuRaj184@users.noreply.github.com>
2024-04-27 22:39:42 +0530

    Update Dropdown.jsx

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,14 +1,14 @@
-import styles from "./css/Dropdown.module.css"
-import { Dropvalue } from "./dropcontext"
+import styles from "./css/Dropdown.module.css" // Importing CSS module styles from Dropdown.module.css
+import { Dropvalue } from "./dropcontext" // Importing the Dropvalue function from the dropcontext file
 
-function Dropdown(){
-    const {hover}= Dropvalue()
+function Dropdown(){ // Defining a functional component named Dropdown
+    const {hover}= Dropvalue() // Destructuring the hover function from the Dropvalue hook
+
     return(
         <>
-            <button onMouseOver={(e)=>hover(e)} className={styles.button}>Select</button>
-        </> 
-        
+            <button onMouseOver={(e)=>hover(e)} className={styles.button}>Select</button> {/* Rendering a button with onMouseOver event listener, triggering the hover function */}
+        </>
     )
 }
 
-export default Dropdown;
+export default Dropdown; // Exporting the Dropdown component as default
